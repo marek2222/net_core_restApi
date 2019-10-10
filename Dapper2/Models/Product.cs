c9127b4b2f3a83f8ab7389dd8524192dc20e8757
marek2222 <marekmusial2@o2.pl>
2019-10-10 12:12:16 +0200

    Dapper2 3.1: Models

---
+++ Dapper2/Models/Product.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Dapper2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
