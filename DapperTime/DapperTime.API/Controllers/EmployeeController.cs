8524f29bf5b53de2ed5b33b34a0ccc673c82517c
marek2222 <marekmusial2@o2.pl>
2019-10-10 09:34:50 +0200

    DapperTime 2.4: I add EmployeeController and a reference to DapperTime.API:
    cd ..
    cd DapperTime.API\
    dotnet add reference ../DapperTime.DATA/DapperTime.DATA.csproj

---
+++ DapperTime/DapperTime.API/Controllers/EmployeeController.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using DapperTime.DATA;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperTime.API.Controllers
+{
+  [Produces("application/json")]
+  [Route("api/Employee")]
+  public class EmployeeController : Controller
+  {
+    private readonly IEmployeeProvider employeeProvider;
+
+    public EmployeeController(IEmployeeProvider employeeProvider)
+    {
+      this.employeeProvider = employeeProvider;
+    }
+
+    // GET: api/Employee
+    [HttpGet]
+    public IEnumerable<Employee> Get()
+    {
+      return employeeProvider.Get();
+    }
+
+    // GET: api/Employee/5
+    [HttpGet("{id}", Name = "Get")]
+    public string Get(int id)
+    {
+      return "value";
+    }
+
+    // POST: api/Employee
+    [HttpPost]
+    public void Post([FromBody]string value)
+    {
+    }
+
+    // PUT: api/Employee/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE: api/ApiWithActions/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
+}
