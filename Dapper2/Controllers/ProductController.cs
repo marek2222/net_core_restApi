23a357cf003ec57541c10ba88a9332bf0a86a898
marek2222 <marekmusial2@o2.pl>
2019-10-10 12:19:55 +0200

    Dapper2 3.1: Creating ProductController

--- Dapper2/Controllers/ProductController.cs
+++ Dapper2/Controllers/ProductController.cs
@@ -1,7 +1,49 @@
+using System.Collections.Generic;
+using Dapper2.Models;
+using Dapper2.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Dapper2.Controllers
 {
-    public class ProductController
+  public class ProductController : ControllerBase
+  {
+    private readonly IProductRepository _productRepository;
+
+    public ProductController(IProductRepository productRepository)
     {
-        
+      _productRepository = productRepository;
     }
+
+    [HttpGet]
+    [Route("{id}")]
+    public ActionResult<Product> GetById(int id)
+    {
+      var product = _productRepository.GetById(id);
+      return Ok(product);
+    }
+    [HttpGet]
+    public ActionResult<List<Product>> GetAll()
+    {
+      var product = _productRepository.GetAllProducts();
+      return Ok(product);
+    }
+    [HttpPost]
+    public ActionResult AddProduct(Product entity)
+    {
+      _productRepository.AddProduct(entity);
+      return Ok(entity);
+    }
+    [HttpPut("{id}")]
+    public ActionResult<Product> Update(Product entity, int id)
+    {
+      _productRepository.UpdateProduct(entity, id);
+      return Ok(entity);
+    }
+    [HttpDelete("{id}")]
+    public ActionResult<Product> Delete(int id)
+    {
+      _productRepository.RemoveProduct(id);
+      return Ok();
+    }
+  }
 }
