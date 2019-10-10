108313b3474a2e260be527b1847f29babad63df5
marek2222 <marekmusial2@o2.pl>
2019-10-10 14:18:30 +0200

    TodoApi 4.1: Dodanie TodoApi i usunięcie ZadanieApi

--- ZadanieApi/Models/Zadanie.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ZadanieApi.Models
-{
-    public class Zadanie
-    {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
-}
--- _ZadanieApi/Models/ZadanieContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace ZadanieApi.Models {
-    public class ZadanieContext : DbContext {
-        public ZadanieContext (DbContextOptions<ZadanieContext> options) : base (options) { }
-        public DbSet<Zadanie> Zadania { get; set; }
-    }
-}
